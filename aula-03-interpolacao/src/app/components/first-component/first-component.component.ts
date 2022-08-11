410b3f4906ec8cf5e41277d848e46d6127200cb6
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-08-10 22:00:11 -0300

    aula-03-interpolacao

--- aula-03-interpolacao/src/app/components/first-component/first-component.component.ts
+++ aula-03-interpolacao/src/app/components/first-component/first-component.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'roberto';
+  age: number = 39;
+  job: string = 'programador';
 
   constructor() { }
 
